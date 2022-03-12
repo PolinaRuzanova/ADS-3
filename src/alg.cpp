c73de363a9355215458497482c80206216af86ec
PolinaRuzanova <94633834+PolinaRuzanova@users.noreply.github.com>
2022-03-12 19:39:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int left = 0;
+    int right = size - 1;
+    int num = 0;
+    int mid;
+    while (right >= left) {
+        mid = (right + left) / 2;
+        if (arr[mid] == value) {
+            num++;
+            break;
+        }
+        if (arr[mid] < value) {
+            left = mid + 1;
+        }
+        if (arr[mid] > value) {
+            right = mid - 1;
+        }
+    }
+    if (num != 0) {
+        for (int i = mid + 1; arr[i] == value && i <= size - 1; i++) {
+            num++;
+        }
+        for (int i = mid - 1; arr[i] == value && i >= 0; i--) {
+            num++;
+        }
+    }
+    return num;
 }
